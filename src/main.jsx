2ce12e9f72b57f965881907e4043c0cd71ea0976
Alicia Gutiérrez <aliciagutierrezdesign@gmail.com>
2025-09-17 09:53:04 +0200

    MyAppForFun added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import MyAppForFun from './MyAppForFun.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <MyAppForFun />
   </StrictMode>,
 )
