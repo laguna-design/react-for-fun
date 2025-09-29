f436da89ce58a5b867eb0d1fac86e7d4681c2cfd
Alicia Gutiérrez <aliciagutierrezdesign@gmail.com>
2025-09-29 10:39:14 +0200

    FilteredProductTable added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
 import MyAppForFun from './MyAppForFun.jsx'
+import FilterableProductTable from './FilterableProductTable.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <MyAppForFun />
+    <FilterableProductTable />
   </StrictMode>,
 )
 
